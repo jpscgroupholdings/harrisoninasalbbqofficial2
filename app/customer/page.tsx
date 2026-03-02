15fbbaf5b5750ff33859569e3b286b07b84c043f
Arandelle <topaguintsarandell@gmail.com>
2026-03-02 15:11:52 +0800

    feat: remove the testimonial

--- app/customer/page.tsx
+++ app/customer/page.tsx
@@ -1,19 +1,15 @@
 import PromoBannerV2 from "@/components/customer/homepage/PromoBannerV2";
 import BestSellers from "@/components/customer/homepage/BestSellers";
-import HeroPromo from "@/components/customer/homepage/HeroPromo";
 import LocationsSection from "@/components/customer/homepage/Location";
 import MenuSection from "@/components/customer/homepage/MenuSection";
 import StickyScrollProducts from "@/components/customer/homepage/StickyScrollProducts";
 import StorySection from "@/components/customer/homepage/StorySection";
-import Testimonials from "@/components/customer/homepage/Testimonials";
 import MenuSectionSkeleton from "@/components/ui/MenuSectionSkeleton";
 import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
-      {/* <HeroPromo /> */}
-      {/* <Hero /> */}
       <PromoBannerV2 />
       <Suspense fallback={<MenuSectionSkeleton />}>
         <MenuSection variant="landing" />
@@ -21,7 +17,6 @@ export default function Home() {
       <StorySection />
       <BestSellers />
       <StickyScrollProducts />
-      <Testimonials />
       <LocationsSection />
     </>
   );
