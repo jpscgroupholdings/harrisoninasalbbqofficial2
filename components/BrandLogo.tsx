7aaf641cd8d95053fb230b740d7fe86fdd0fc8d6
Arandelle <topaguintsarandell@gmail.com>
2026-03-26 17:05:52 +0800

    feat: add cn to make it reusable

--- components/BrandLogo.tsx
+++ components/BrandLogo.tsx
@@ -1,22 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { cn } from "@/lib/utils";
 
-const BrandLogo = ({color = 'normal'} : {color?: "white" | "normal"}) => {
+const BrandLogo = ({
+  color = "normal",
+  className,
+}: {
+  color?: "white" | "normal";
+  className?: string;
+}) => {
   return (
-    <Link
-      href={"/"}
-      className="cursor-pointer"
-    >
+    <Link href={"/"} className="cursor-pointer">
       <div className="flex items-center">
         <Image
-          src={color === 'normal' ? "/images/harrison_logo_landscape.png" : "/images/harrison_logo_landscape_white.png"}
+          src={
+            color === "normal"
+              ? "/images/harrison_logo_landscape.png"
+              : "/images/harrison_logo_landscape_white.png"
+          }
           alt="Harrison"
           width={240}
           height={60}
           priority
-          quality={92} // tiny bump since smaller size = more perceived sharpness
-          className="h-8 w-auto md:h-10 lg:h-12" // 32px → 40px → 48px
+          quality={92}
+          className={cn("h-8 w-auto md:h-10 lg:h-12", className)}
           sizes="(max-width: 500px) 120px, (max-width: 1024px) 180px, 240px"
         />
       </div>
