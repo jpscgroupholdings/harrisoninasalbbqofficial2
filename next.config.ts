6e0b0ef6391acc99bd2f195dfa37287077ab2eee
Arandelle <topaguintsarandell@gmail.com>
2026-03-28 18:56:51 +0800

    feat: revert

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
-  output: "export",
   turbopack: {}, // 👈 silences the error
   images: {
     remotePatterns: [
