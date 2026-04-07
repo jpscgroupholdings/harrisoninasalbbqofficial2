c9baaf882cddaf52377102c824753d10eab99d4c
Arandelle <topaguintsarandell@gmail.com>
2026-04-07 10:42:11 +0800

    feat: Loading page component

---
+++ components/ui/LoadingPage.tsx
@@ -0,0 +1,12 @@
+const LoadingPage = () => {
+  return (
+    <div className="absolute inset-0 z-50 flex flex-col items-center justify-center h-screen">
+      <img
+        src="/gif/loading-animation.gif"
+        alt="loading"
+      />
+    </div>
+  );
+};
+
+export default LoadingPage;
