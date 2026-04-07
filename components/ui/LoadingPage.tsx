9379c314a14195720c32b652c2e701b5557525ca
Arandelle <topaguintsarandell@gmail.com>
2026-04-07 15:30:45 +0800

    feat: h-full instead of screen

--- components/ui/LoadingPage.tsx
+++ components/ui/LoadingPage.tsx
@@ -1,6 +1,6 @@
 const LoadingPage = () => {
   return (
-    <div className="absolute inset-0 z-50 flex flex-col items-center justify-center h-screen">
+    <div className="absolute inset-0 z-50 flex flex-col items-center justify-center h-full">
       <img
         src="/gif/loading-animation.gif"
         alt="loading"
