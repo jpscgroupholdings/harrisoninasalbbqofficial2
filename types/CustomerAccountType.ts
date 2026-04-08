450a8c7da36ba8e36661f7fe79009e51bf6c535d
Arandelle <topaguintsarandell@gmail.com>
2026-04-08 18:02:16 +0800

    feat: add totalspent and orders in types

--- types/CustomerAccountType.ts
+++ types/CustomerAccountType.ts
@@ -1,20 +1,35 @@
 export type Customer = {
-    _id?: string,
-    fullname: string,
-    phone?: string,
-    email: string,
-    password: string,
-    isActive: boolean,
-    createdAt?: string 
-}
+  _id?: string;
+  fullname: string;
+  phone?: string;
+  email: string;
+  password: string;
+  isActive: boolean;
+  createdAt?: string;
+};
 
 export type CustomerCreateInput = {
-    fullname: string,
-    phone?: string,
-    email: string,
-    password: string
-}
+  fullname: string;
+  phone?: string;
+  email: string;
+  password: string;
+};
 
-export type CustomerUpdateInput = Partial<CustomerCreateInput>
+export type CustomerUpdateInput = Partial<CustomerCreateInput>;
 
-export type CustomerResponse = Omit<Customer, "password">
+export type CustomerResponse = Omit<Customer, "password"> & {
+  totalSpent: number;
+  totalOrders: number;
+};
+
+export type CustomersListResponse = {
+  data: CustomerResponse[];
+  pagination: {
+    total: number;
+    page: number;
+    limit: number;
+    totalPages: number;
+    hasNext: boolean;
+    hasPrev: boolean;
+  };
+};
