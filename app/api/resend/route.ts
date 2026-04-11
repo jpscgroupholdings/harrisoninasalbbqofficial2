50a7814cd309bb4600981f973cfa00244114cd90
Arandelle <topaguintsarandell@gmail.com>
2026-04-11 18:08:02 +0800

    feat: api route to send an email from resend

---
+++ app/api/resend/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import { resend, EMAIL_FROM } from "@/lib/resend";
+import { OtpEmail } from "@/app/emails/OTPEmail";
+import crypto from "crypto";
+
+export async function POST(req: NextRequest) {
+  const { to, name } = await req.json();
+
+  if (!to || !name) {
+    return NextResponse.json({ error: "Missing 'to' field" }, { status: 400 });
+  }
+
+  const otp = crypto.randomInt(100000, 999999).toString();
+
+  const { data, error } = await resend.emails.send({
+    from: EMAIL_FROM,
+    to,
+    subject: "One-Time Verification",
+    react: OtpEmail({
+        username: name,
+        otpCode: otp,
+        expiryMinutes: 10
+    })
+  });
+
+  if (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+
+  console.log(error)
+
+  return NextResponse.json({ success: true, id: data?.id });
+}
