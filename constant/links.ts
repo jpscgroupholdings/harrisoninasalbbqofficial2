01ec2a390ab486460f09067273ab4e7c3c1badf6
Arandelle <topaguintsarandell@gmail.com>
2026-04-21 15:58:09 +0800

    feat: update route for harrison

--- constant/links.ts
+++ constant/links.ts
@@ -1,5 +1,5 @@
 export const LINKS = {
-    MENU: "/menu",
+    MENU: "/",
     GRAB: "https://r.grab.com/g/6-20251008_170627_D2CDC68C6B784515B161E8C1D2AA2F79_MEXMPS-2-C7NFCKJ2G3UVTT",
     FOODPANDA: "https://foodpanda.go.link/1GiOQ",
     KINGS_COURT_LINK: "https://maps.app.goo.gl/iKy7TX6hLx6XnooH9",
