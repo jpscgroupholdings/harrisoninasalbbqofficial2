7410b162b16679a6ac0aafbe4fcca6549f806f4c
Arandelle <topaguintsarandell@gmail.com>
2026-04-27 12:07:12 +0800

    feat: add cn for dynamic classname

--- components/ui/LoadingPage.tsx
+++ components/ui/LoadingPage.tsx
@@ -1,10 +1,14 @@
-const LoadingPage = () => {
+import { cn } from "@/lib/utils";
+
+const LoadingPage = ({className} : {className?: string}) => {
   return (
-    <div className="absolute inset-0 z-50 flex flex-col items-center justify-center h-full">
-      <img
-        src="/gif/loading-animation.gif"
-        alt="loading"
-      />
+    <div className={cn(`h-screen`,className)}>
+      <div className={`absolute inset-0 z-50 flex flex-col items-center justify-center h-full`}>
+        <img
+          src="/gif/loading-animation.gif"
+          alt="loading"
+        />
+      </div>
     </div>
   );
 };
