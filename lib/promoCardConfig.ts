0915cdf7fe0e441d7197a2b74ed3cc11cc424296
Arandelle <topaguintsarandell@gmail.com>
2026-05-30 11:03:06 +0800

    feat: create promo card config for discount rules

---
+++ lib/promoCardConfig.ts
@@ -0,0 +1,23 @@
+import {
+  DEFAULT_PROMO_CARD_DISCOUNT_RULES,
+  DEFAULT_PROMO_CARD_VOUCHER_RULE,
+  PROMO_CARD,
+  PromoCardConfig,
+} from "@/lib/promoCard";
+import { PromoCardConfigModel } from "@/models/PromoCardConfig";
+
+export async function getPromoCardConfig(): Promise<PromoCardConfig> {
+  const config = await PromoCardConfigModel.findOne().lean<PromoCardConfig>();
+
+  return {
+    name: config?.name ?? PROMO_CARD.name,
+    discountRate: config?.discountRate ?? PROMO_CARD.discountRate,
+    purchasePrice: config?.purchasePrice ?? PROMO_CARD.purchasePrice,
+    sku: config?.sku ?? PROMO_CARD.sku,
+    discountRules:
+      config?.discountRules?.length
+        ? config.discountRules
+        : DEFAULT_PROMO_CARD_DISCOUNT_RULES,
+    voucherRule: config?.voucherRule ?? DEFAULT_PROMO_CARD_VOUCHER_RULE,
+  };
+}
