278bb1851f101ea645d3b82bb1be801c76d71440
Arandelle <topaguintsarandell@gmail.com>
2026-06-10 16:03:32 +0800

    feat: add header

--- app/admin/(protected)/promotions/(pages)/bundle-discounts/new/page.tsx
+++ app/admin/(protected)/promotions/(pages)/bundle-discounts/new/page.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+'use client';
+
+import SectionHeader from "@/app/admin/components/SectionHeader";
 
 const page = () => {
   return (
-    <div>
-      create bundle item
+    <div className="space-y-6">
+      <SectionHeader
+        title="Create Bundle Discount"
+        subTitle="Manage your bundle discount - either same items, different items, or combo items"
+      />
+      
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
