8bd0766f6b6b7d93f3e5272ac2600ff8acbad1eb
Arandelle <topaguintsarandell@gmail.com>
2026-06-10 15:59:01 +0800

    feat: add bundle type types for the bundle discount promotion

--- types/promotions/bundle-discount.type.ts
+++ types/promotions/bundle-discount.type.ts
@@ -6,12 +6,22 @@ import {
 } from "./promotion-constant";
 import { ProductDiscountProductSnapshot } from "./product-discount.type";
 
+export const BUNDLE_TYPE = {
+  SAME_ITEMS : "same_items",
+  ANY_ITEMS: "any_items",
+  COMBO_ITEMS: "combo_items"
+} as const;
+
+export type BundleType = (typeof BUNDLE_TYPE)[keyof typeof BUNDLE_TYPE];
+
 export type BundleDiscountProductSnapshot = ProductDiscountProductSnapshot & {
   quantity: number;
 };
 
-type BundleDiscountPromotionConfig = PromotionConfig & {
+export type BundleDiscountPromotionConfig = PromotionConfig & {
   promotionType: typeof PROMOTION_TYPES.BUNDLE_DISCOUNT;
+  bundleType: BundleType;
+  requiredQuantity?: number | null;
   products: BundleDiscountProductSnapshot[];
   categoryIds: string[];
 };
@@ -21,6 +31,8 @@ export const DEFAULT_BUNDLE_PROMOTION_DISCOUNT: BundleDiscountPromotionConfig =
     ...DEFAULT_PROMOTION_RULES,
     ...DEFAULT_PROMOTION_DISCOUNT,
     promotionType: PROMOTION_TYPES.BUNDLE_DISCOUNT,
+    bundleType: BUNDLE_TYPE.ANY_ITEMS,
+    requiredQuantity: null,
     name: "Bundle Promotion",
     products: [],
     categoryIds: [],
