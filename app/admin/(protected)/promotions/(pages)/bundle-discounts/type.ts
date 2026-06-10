7d42c1fff6db88d6842e0e0a83bd517d7931e3ee
Arandelle <topaguintsarandell@gmail.com>
2026-06-10 16:03:04 +0800

    feat: add attriutes bundleType and requiredQuantity

--- app/admin/(protected)/promotions/(pages)/bundle-discounts/type.ts
+++ app/admin/(protected)/promotions/(pages)/bundle-discounts/type.ts
@@ -1,9 +1,17 @@
-import { PROMOTION_TYPES } from "@/types/promotions/promotion-constant";
-import { AdminPromotionBase, ProductSnapshot } from "../../types/discount-promotion.type";
+import {
+  PROMOTION_TYPES,
+} from "@/types/promotions/promotion-constant";
+import {
+  AdminPromotionBase,
+  ProductSnapshot,
+} from "../../types/discount-promotion.type";
+import { type BundleType } from "@/types/promotions/bundle-discount.type";
 
 export type BundleDiscountPromotion = AdminPromotionBase & {
+  bundleType: BundleType;
   promotionType: typeof PROMOTION_TYPES.BUNDLE_DISCOUNT;
   products: (ProductSnapshot & { quantity: number })[];
+  requiredQuantity?: number | null;
 };
 
 export type BundleDiscountPromotionsResponse = {
