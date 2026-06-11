07134e6c175f37f068d7d31c23f3fe6579de91bf
Arandelle <topaguintsarandell@gmail.com>
2026-06-11 11:26:16 +0800

    feat: form and payload type for the bundle promotion

--- app/admin/(protected)/promotions/(pages)/bundle-discounts/type.ts
+++ app/admin/(protected)/promotions/(pages)/bundle-discounts/type.ts
@@ -1,11 +1,11 @@
-import {
-  PROMOTION_TYPES,
-} from "@/types/promotions/promotion-constant";
+import { PROMOTION_TYPES } from "@/types/promotions/promotion-constant";
 import {
   AdminPromotionBase,
+  AdminPromotionForm,
+  AdminPromotionSavePayload,
   ProductSnapshot,
 } from "../../types/discount-promotion.type";
-import { type BundleType } from "@/types/promotions/bundle-discount.type";
+import type { BundleType } from "@/types/promotions/bundle-discount.type";
 
 export type BundleDiscountPromotion = AdminPromotionBase & {
   bundleType: BundleType;
@@ -20,4 +20,16 @@ export type BundleDiscountPromotionsResponse = {
 
 export type BundleDiscountPromotionMutationResponse = {
   promotion: BundleDiscountPromotion;
+};
+
+export type BundleDiscountPromotionForm = AdminPromotionForm & {
+  bundleType: BundleType;
+  requiredQuantity: string;
+  productQuantities: Record<string, string>;
+};
+
+export type BundleDiscountPromotionSavePayload = AdminPromotionSavePayload & {
+  bundleType: BundleType;
+  requiredQuantity: number | null;
+  productQuantities: Record<string, number>;
 };
