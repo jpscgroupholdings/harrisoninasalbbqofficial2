d9d730e462c3f7f7b6ee0a548a65c6239c2277e7
Arandelle <topaguintsarandell@gmail.com>
2026-06-13 18:16:51 +0800

    update(admin protected layout): remove the staff provider as it is used on root admin layout

--- app/admin/(protected)/layout.tsx
+++ app/admin/(protected)/layout.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import ClienLayout from "./ClientLayout";
-import { StaffProvider } from "@/contexts/StaffContext";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-      <StaffProvider>
-        <ClienLayout>{children}</ClienLayout>
-      </StaffProvider>
-  );
+  return <ClienLayout>{children}</ClienLayout>;
 };
 
 export default layout;
-  
