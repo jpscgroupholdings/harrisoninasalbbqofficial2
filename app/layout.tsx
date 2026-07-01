9bb27f65fdf946922293ba0d526b481a628f211b
Arandelle <topaguintsarandell@gmail.com>
2026-07-01 18:24:00 +0800

    feat: script to detect the user behavior using the meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { larken, syne } from "./font";
 import { Suspense } from "react";
 import LoadingPage from "@/components/ui/LoadingPage";
 
+import Script from "next/script";
+
 export const metadata: Metadata = {
   title: "Home | Harrison House of Inasal & BBQ",
   description:
@@ -51,7 +53,7 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: "images/harrison_logo.png",
+    icon: "/images/harrison_logo.png",
   },
 };
 
@@ -77,6 +79,36 @@ export default function RootLayout({
           <Suspense fallback={<LoadingPage />}>{children}</Suspense>
           {modal}
         </QueryProvider>
+
+          <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+        >
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1584332956581235');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1584332956581235&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );
