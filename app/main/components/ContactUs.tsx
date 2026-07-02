d63ff60986db2541fe63ec119c1be119422ed730
Arandelle <topaguintsarandell@gmail.com>
2026-07-02 11:15:41 +0800

    fix: add use client

--- app/main/components/ContactUs.tsx
+++ app/main/components/ContactUs.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from "react";
 import { InputField } from "@/components/ui/FormComponents/InputField";
 import { SelectField } from "@/components/ui/FormComponents/SelectField";
