10833443761a29ecd6bae5e34044cea93f0a6cda
Arandelle <topaguintsarandell@gmail.com>
2026-07-03 15:44:37 +0800

    feat: add  tabCounts for response type and paymentFilter on order params type

--- hooks/api/shared/useOrdersBase.ts
+++ hooks/api/shared/useOrdersBase.ts
@@ -14,6 +14,7 @@ import { toast } from "sonner";
 interface ProductResponse {
   data: OrderType[];
   pagination: PaginationMeta;
+  tabCounts?: Record<string, number>;
 }
 
 const ORDER_ENDPOINTS = {
@@ -28,6 +29,7 @@ export type OrderParams = {
   sort?: string;
   search?: string;
   status?: OrderStatus | OrderStatus[];
+  paymentFilter?: "confirmed" | "unpaid";
   branchId?: "all" | string;
   productType?: string;
 };
