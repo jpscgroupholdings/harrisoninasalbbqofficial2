78aa9de61d67fc8dd17b38fca9f42313f6ac4429
Arandelle <topaguintsarandell@gmail.com>
2026-07-08 18:06:39 +0800

    refactor(getAPIError): add options arguments for extra error message and fallback message

--- lib/getApiError.ts
+++ lib/getApiError.ts
@@ -1,19 +1,30 @@
 import { NextResponse } from "next/server";
+
+interface APIErrorOptions {
+  fallbackMessage?: string;
+  extra?: Record<string, unknown>;
+}
+
 /**
  *
  * @param error - Error message you want to tell to the user
  * @param statusCode - status code of it e.g - 401, 404, 500
- * @param fallbackMessage - incase error message failed
+ * @param options - optional fallbackMessage and exta response fields
  * @returns
  */
 export function getAPIError(
   error: unknown,
   statusCode: number = 500,
-  fallbackMessage = "Internal Server Error",
+  options: APIErrorOptions = {},
 ) {
+  const { fallbackMessage = "Internal Server Error", extra } = options;
+
   const err = typeof error === "string" ? new Error(error) : error;
   console.error(error);
 
   const message = err instanceof Error ? err.message : fallbackMessage;
-  return NextResponse.json({ error: message }, { status: statusCode });
+  return NextResponse.json(
+    { error: message, ...extra },
+    { status: statusCode },
+  );
 }
